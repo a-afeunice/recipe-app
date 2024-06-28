c1d803c46458450738efaffef6a2b14fdea58df0
a-afeunice <afrifae2@gmail.com>
2024-06-28 12:07:43 +0000

    React Router

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+        <h3>Savory & Sweets</h3>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Welcome
